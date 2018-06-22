0b9b16671b323e6e342751530cb46a01c774a952
Leonardo Pagnez <leopagnez@gmail.com>
2018-06-22 16:39:22 -0300

    Adding tables.sql to proper maintain documentation

---
+++ WEB-INF/src/DeploymentServlet/ConfigServlet.java
@@ -0,0 +1,19 @@
+package DeploymentServlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class ConfigServlet extends HttpServlet {
+    private Object username = null;
+
+
+    @Override
+    public void init() throws ServletException {
+    }
+
+    public void destroy() {
+
+    }
+
+}
--- WEB-INF/src/SchedulerServlets/UserSpaceRebuild.java
+++ WEB-INF/src/SchedulerServlets/UserSpaceRebuild.java
@@ -1,9 +1,125 @@
 package SchedulerServlets;
 
+import pkg1.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class UserSpaceRebuild implements Runnable{
+
+    private static HttpSession session = null;
+    private HTMLBuilder htmlBuild;
+    private Sistema sist;
+    private MysqlConnect mysql;
+    private HTMLTarefasBuilder htmlTarBuild;
+    private HTMLBuilderReuniao htmlReubuild;
+    private String Name =null;
+    private ServletContext context;
+
+    public UserSpaceRebuild(ServletContext Le_context){
+        context = Le_context;
+    }
+
+
+    private void loadTarefas()throws SQLException {
+        String querry = "Select definicao from tarefas";
+        ArrayList<String> str = mysql.sqlSelect(querry, 1);
+        if(str.size() > 0){
+            for(int i = 0; i < str.size(); i++) {
+                Tarefa task = new Tarefa(str.get(i));
+                sist.getSistTarefa().addTarefa(task);
+            }
+
+        }
+
+        String querry1 = "Select definicao from tarefas where progressoTotal < 100";
+        ArrayList<String> str1 = mysql.sqlSelect(querry1, 1);
+        if(str1.size() > 0){
+            for(int i = 0; i < str1.size(); i++) {
+                Tarefa task = new Tarefa(str1.get(i));
+                sist.getSistTarefa().addTarefaPendente(task);
+            }
+
+        }
+    }
+
+    private void loadMembros()throws SQLException {
+        String querry = "Select nome,username,funcao from membros where status = 1 AND username <> '" + sist.getMembro().getUsername() + "'";
+        ArrayList<String> str = mysql.sqlSelect(querry, 3);
+        if(str.size() > 0){
+            for(int i = 0; i < str.size(); i=i+3) {
+                Membro m = new Membro(str.get(i),str.get(i+2),str.get(i+1),Boolean.TRUE);
+                sist.getSistComunicacao().addMembroOnline(m);
+            }
+        }
+
+        String querry2 = "Select nome,username,funcao from membros where status = 0";
+        ArrayList<String> str2 = mysql.sqlSelect(querry2, 3);
+        if(str2.size() > 0){
+            for(int i = 0; i < str2.size(); i=i+3) {
+                Membro m = new Membro(str2.get(i),str2.get(i+2),str2.get(i+1),Boolean.FALSE);
+                sist.getSistComunicacao().addMembroOffline(m);
+            }
+        }
+
+    }
+
+    private void loadReunioes()throws SQLException {
+        String querry = "Select ID,data,local,pauta from reuniao";
+        ArrayList<String> str = mysql.sqlSelect(querry, 4);
+        if(str.size() > 0){
+            for(int i = 0; i < str.size(); i=i+4) {
+                Reuniao r = new Reuniao(Integer.valueOf(str.get(i)),str.get(i+1),str.get(i+2),str.get(i+3));
+                sist.getSistReuniao().addReuniao(r);
+            }
+
+        }
+    }
+
+    private Boolean rebuildHome(){
+        int i =0;
+        Boolean flag = Boolean.FALSE;
+        try{
+            mysql = sist.getMysql();
+            String querry = "Select nome,username,funcao from membros where status = 1";
+            ArrayList<String> str = mysql.sqlSelect(querry, 3);
+            while(i < str.size()) {
+                flag = Boolean.TRUE;
+                Name = str.get(0);
+                Membro usr = new Membro(str.get(0),str.get(2), str.get(1), Boolean.TRUE);
+                sist.setMembro(usr);
+                this.loadMembros();
+                this.loadReunioes();
+                this.loadTarefas();
+                htmlBuild = sist.getHtmlBuild();
+                htmlTarBuild = sist.getHtmlTarefasBuilder();
+                htmlReubuild = sist.getHtmlReuniaoBuilder();
+                ArrayList<Tarefa> tar = sist.getSistTarefa().getTarefas();
+                ArrayList<Membro> off =sist.getSistComunicacao().getMembrosOffline();
+                ArrayList<Membro> on =sist.getSistComunicacao().getMembrosOnline();
+                ArrayList<Tarefa> pend = sist.getSistTarefa().getTarefasPendents();
+                ArrayList<Reuniao> reun = sist.getSistReuniao().getReunioes();
+                htmlBuild.makeHome(on, off, pend, Name);
+                htmlTarBuild.makeHome(tar, Name);
+                htmlReubuild.makeHome(reun, Name);
+                sist.getSistComunicacao().setMembrosOffline(new ArrayList<Membro>());
+                sist.getSistComunicacao().setMembrosOnline(new ArrayList<Membro>());
+                sist.getSistTarefa().setTarefasPendente(new ArrayList<Tarefa>());
+                sist.getSistTarefa().setTarefas(new ArrayList<Tarefa>());
+                sist.getSistReuniao().setReunioes(new ArrayList<Reuniao>());
+                i = i + 3;
+            }
+        }catch(Exception e){
+            flag = Boolean.FALSE;
+        }
+        return flag;
+    }
 
     @Override
     public void run(){
-
+        try{ sist = Sistema.getInstance(); }catch(Exception excpt){ excpt.printStackTrace(); }
+        rebuildHome();
     }
 }
